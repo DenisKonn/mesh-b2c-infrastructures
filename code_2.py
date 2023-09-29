8188391a9c199dda491afde7520b3ded61927dba
DenisKonn <140009429+DenisKonn@users.noreply.github.com>
2023-09-29 19:55:17 +0200

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,42 @@
+        console.log("Random Number: " + item);
+
+}
+    });
+
+function generateRandomData() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    return data;
+
+    data.forEach(item => {
+    const data = [];
+    for (let i = 0; i < 10; i++) {
+main();
+    }
+function main() {
+}
+    const data = generateRandomData();    public static void main(String[] args) {
+            System.out.println("Random Number: " + item);
+    }
+        }
+        for (int item : data) {
+}
+public class RandomDataGenerator {
+
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int i = 0; i < 10; i++) {
+
+        }
+        int[] data = new int[10];
+def generate_random_data():
+    return data
+
+import random
+        print(f"Random Number: {item}")
+    for item in data:
+
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+if __name__ == "__main__":
+
+def main():
